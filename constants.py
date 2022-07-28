9d40cf658b49975687f625fe0ed4af5c2e1b8309
Akex <67469268+akex06@users.noreply.github.com>
2022-07-28 14:34:28 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+#   DISCORD
+
+#   CAN BE FOUND IN https://discord.com/developers
+DISCORD_TOKEN = TOKEN"
+API_URL = "https://discord.com/api"
+#   DISCORD CHANNEL WHERE MESSAGES WILL BE SEND
+CHANNEL_ID = 000000000000000000
+
+#   TWITCH
+
+#   CAN BE GENERATED AT https://twitchtokengenerator.com/
+TWITCH_TOKEN = "TWITCH_TOKEN"
+#   TWITCH CHANNEL NAME
+CHANNEL = ["CHANNEL_NAME"]
